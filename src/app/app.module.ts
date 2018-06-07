9fd8e2a943f818dedf1646e4ccf73a0f0a746f36
Faith <faith.mustapha@andela.com>
2018-06-07 16:25:14 +0100

    add navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { SliderComponent } from './slider/slider.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SliderComponent
+    SliderComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
